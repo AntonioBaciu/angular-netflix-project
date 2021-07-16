c725d5d2a453f27ccd6d0935b03fc641a5685ff2
AntonioBaciu <antoniopaulobaciu@gmail.com>
2021-07-16 09:46:02 +0200

    Looping with *ngFor in order to get all card data

--- angular-netflix-project/src/app/app.component.ts
+++ angular-netflix-project/src/app/app.component.ts
@@ -1,16 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-// interface Movie {
-//   image: String;
-//   name: String;
-//   releaseYear: String;
-//   genre: String;
-//   duration: String;
-//   actors: String;
-//   storyLine: String;
-// }
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
